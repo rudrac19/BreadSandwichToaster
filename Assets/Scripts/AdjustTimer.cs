0883016d1fa841a0bb367819a66cf53dc658701a
Rudra Pratap Chauhan <rcpython2@gmail.com>
2024-12-15 19:08:54 -0500

    almost done with timer mode
    (beta 1.2.4)

--- Assets/Scripts/AdjustTimer.cs
+++ Assets/Scripts/AdjustTimer.cs
@@ -2,22 +2,95 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class AdjustTimer : MonoBehaviour
 {
     public Text time;
     [SerializeField] Slider slider;
+    [SerializeField] GameObject sliderObject;
+    [SerializeField] GameObject[] buttons;
+    [SerializeField] GameObject[] editableUIs;
+
+    public int minutes;
+    public int seconds;
+
+    public bool editable;
+
+    void Start()
+    {
+        editable = true;
+        sliderObject.SetActive(true);
+        foreach (var button in buttons)
+        {
+            button.SetActive(false);
+        }
+        foreach (var editableUI in editableUIs)
+        {
+            editableUI.SetActive(true);
+        }
+    }
 
     void Update()
     {
-        time.text = Translate(slider.value);
+        if (editable)
+        {
+            sliderObject
+                .SetActive(true);
+            float x = slider.value;
+
+            int min = (int)x / 60;
+            int sec = (int)x % 60;
+
+            minutes = min; // For global use only
+            seconds = sec; // For global use only
+
+
+            time.text = min.ToString("D2") + ":" + sec.ToString("D2");
+        } else
+        {
+            editable = false;
+            sliderObject.SetActive(false);
+            foreach (var button in buttons)
+            {
+                button.SetActive(true);
+            }
+            foreach (var editableUI in editableUIs)
+            {
+                editableUI.SetActive(false);
+            }
+            time.text = minutes.ToString("D2") + ":" + seconds.ToString("D2");
+        }
+    }
+    
+    public void PerpareCountdown()
+    {
+        editable = false;
+        StartCoroutine(StartCountdown());
     }
 
-    void Translate(float x, out string minutes, out string seconds)
+    IEnumerator StartCountdown()
     {
-        int a = (int)x / 60;
-        int b = (int)x % 60;
-        var c = a.ToString("D2") + ":" + b.ToString("D2");
-        return c;
+        yield return new WaitForSeconds(1f);
+        int sum = minutes + seconds;
+
+        if (sum != 0)
+        {
+            if (seconds == 0)
+            {
+                minutes -= 1;
+                seconds = 60;
+                seconds -= 1;
+            }
+            else
+            {
+                seconds -= 1;
+            }
+            StartCoroutine(StartCountdown());
+        }
+        else
+        {
+            //end countdown
+        }
     }
 }
